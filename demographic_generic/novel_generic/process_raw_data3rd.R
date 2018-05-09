dd5d9a282d1b54a1d653b04bd18016273eb66c8d
Xiuyuan Zhang <xiuyuan1028@gmail.com>
2018-05-09 17:53:54 -0500

    add 3rd data analysis

---
+++ demographic_generic/novel_generic/process_raw_data3rd.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(lubridate)
+
+raw_data <- read_csv('data/qualtrics_3rd_demographic_novel_generic_200.csv') 
+turk_200_data <- read_csv('data/mturk_3rd_demographic_novel_generic_200.csv')
+
+de_identified_data <- raw_data %>%
+  slice(-1:-2) %>%
+  filter(!is.na(MTurkCode)) %>%
+  mutate(StartDate = ymd_hms(StartDate)) %>%
+  arrange(MTurkCode, StartDate) %>%
+  distinct(MTurkCode, .keep_all = TRUE) %>%
+  filter(MTurkCode %in% turk_200_data$Answer.surveycode) %>%
+  mutate(id = 1:n()) %>%
+  select(-IPAddress, -ResponseId, -LocationLatitude, -LocationLongitude, -MTurkCode)
+
+
+write_csv(de_identified_data, "data/05092018_novel_anonymized_data.csv")
